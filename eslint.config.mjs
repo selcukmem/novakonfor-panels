d7a64f5728c1ff7a630ba83ec46a145d626fc067
selcukmem <navikonfor@gmail.com>
2025-09-02 20:27:08 +0300

    eslint no-explicit-any kuralı kapatıldı

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // "any" hatalarını kapat
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
